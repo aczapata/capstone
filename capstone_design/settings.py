7877360608c4ceeec635065d07f70277c38e24ad
aczapata <andreaczapata@gmail.com>
2016-05-12 21:06:07 -0500

    Fix staticfiles

--- capstone_design/settings.py
+++ capstone_design/settings.py
@@ -15,10 +15,10 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'staticfiles'),
